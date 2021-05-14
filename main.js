c8a6bcb68faffd0a7ce31359b0b8bb0476f8636c
kevas007 <kevassiobo@gmail.com>
2021-05-14 07:56:12 +0200

    base

---
+++ main.js
@@ -0,0 +1,22 @@
+let titre = document.getElementsByTagName('h1');
+console.log(titre);
+
+let paragraphe = document.getElementsByTagName('p');
+
+console.log(paragraphe);
+
+let titre2 = document.getElementsByTagName('h3');
+
+console.log(titre2);
+
+let para = paragraphe[1];
+
+console.log(para);
+
+let liste = document.getElementsByTagName('li');
+
+console.log(liste);
+
+let liste3 = liste[2];
+
+console.log(liste3);
